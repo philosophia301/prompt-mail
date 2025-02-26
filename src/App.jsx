24ff7fc4773bb36fdd5f9c8aea524d8555d63192
philosophia301 <eunjong147@gmail.com>
2025-02-26 19:09:17 +0900

    css 교통정리, prettier 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
-import Header from './Header'
-import Hero from './Hero'
-import Upload from './Upload'
-import Prompt from './Prompt'
-import Result from './Result'
+import { useState } from "react";
+import Header from "./Header";
+import Hero from "./Hero";
+import Upload from "./Upload";
+import Prompt from "./Prompt";
+import Result from "./Result";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('hero')
+  const [currentPage, setCurrentPage] = useState("hero");
 
   return (
-    <div className='size-full min-w-screen min-h-screen flex flex-1'>
-      <Header />
-      {currentPage === 'hero' && <Hero setCurrentPage={setCurrentPage} />}
-      {currentPage === 'upload' && <Upload setCurrentPage={setCurrentPage} />}
-      {currentPage === 'prompt' && <Prompt setCurrentPage={setCurrentPage} />}
-      {currentPage === 'result' && <Result setCurrentPage={setCurrentPage} />}
+    <div className="w-[100vw] h-[100vh] flex flex-1 flex-col">
+      <Header setCurrentPage={setCurrentPage} />
+      <div className="flex flex-1 w-[80%] max-w-[960px] mx-auto">
+        {currentPage === "hero" && <Hero setCurrentPage={setCurrentPage} />}
+        {currentPage === "upload" && <Upload setCurrentPage={setCurrentPage} />}
+        {currentPage === "prompt" && <Prompt setCurrentPage={setCurrentPage} />}
+        {currentPage === "result" && <Result setCurrentPage={setCurrentPage} />}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import heroImage from './assets/hero-image.png'
+import { useState } from "react";
+import heroImage from "./assets/hero-image.png";
 
 export default function Hero({ setCurrentPage }) {
-    return (
-        <div className='flex flex-1 w-full h-screen'>
-            <div className='flex flex-1 w-full h-full p-40'>
-                <div className='w-full rounded-xl bg-cover bg-center' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url(${heroImage})` }}>
-                    <div className='flex flex-col h-full gap-2 px-10 py-8 rounded-xl text-white items-start justify-end'>
-                        <h1 className='text-4xl font-black leading-tight tracking-[-0.015em]'>Generate custom AI writing prompts from your photos</h1>
-                        <h2 className='text-sm font-normal leading-normal'>Boost your creativity with unique, personalized prompts. Just upload a photo and let our AI do the rest.</h2>
-                        <button className='flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#308ce8] text-white text-sm font-bold leading-normal tracking-[0.015em]' onClick={() => setCurrentPage('upload')}>
-                            <span className='truncate'>Get Started</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <div
+        className="flex flex-1 rounded-xl bg-cover bg-center min-h-[500px] justify-end"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.4) 100%), url(${heroImage})`,
+        }}
+      >
+        <div className="flex flex-1 flex-col gap-2 p-10 rounded-xl text-white items-start justify-end">
+          <h1 className="text-4xl font-black leading-tight tracking-[-0.015em]">
+            Generate custom AI writing prompts from your photos
+          </h1>
+          <h2 className="text-sm font-normal leading-normal">
+            Boost your creativity with unique, personalized prompts. Just upload
+            a photo and let our AI do the rest.
+          </h2>
+          <button
+            className="flex min-w-[84px] max-w-[480px] mt-2 cursor-pointer items-center justify-center overflow-hidden rounded-full h-8 px-4 bg-[#308ce8] text-sm font-bold tracking-[0.015em]"
+            onClick={() => setCurrentPage("upload")}
+          >
+            <span className="truncate">Get Started</span>
+          </button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
